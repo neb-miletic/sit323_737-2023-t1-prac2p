733106b3159dfa345e51162c80d594ec220bc258
neb-miletic <nebojse@gmail.com>
2023-03-17 21:26:02 +1100

    Change for Week 1 project

---
+++ index.js
@@ -0,0 +1,13 @@
+let exoress = require('express')
+
+let app = exoress()
+
+//respond with 'Hello world' when GET request is made to the homepage
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+// Listen to a particular port
+
+app.listen(3000)
